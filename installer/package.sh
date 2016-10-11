068c883ce78caa0224eaff0e095bbe84610c37f6
Jason Champion <jchampion@zetacentauri.com>
2016-10-11 13:32:56 -0700

    Add patch copy to package command.

--- installer/package.sh
+++ installer/package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp -R ../Release/Patches Sigmatizm.app/Contents/Resources/
 codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" Sigmatizm.app/Contents/Frameworks/libportaudio.2.dylib --entitlements Sigmatizm.app/Contents/Resources/Entitlements.plist
 codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" Sigmatizm.app --entitlements Sigmatizm.app/Contents/Resources/Entitlements.plist
 productbuild --sign "3rd Party Mac Developer Installer: Jason Champion (G6PFF85X2R)" --component Sigmatizm.app /Applications Sigmatizm.pkg
