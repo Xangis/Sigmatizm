50a977bc344859498642febdd2c2ccd1ed42da0c
Jason Champion <jchampion@zetacentauri.com>
2016-10-10 18:24:27 -0700

    Placeholder (untested) package script for osx.

---
+++ installer/package.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" Sigmatizm.app/Contents/Frameworks/libportaudio.2.dylib --entitlements Sigmatizm.app/Contents/Resources/Entitlements.plist
+codesign -f -s "3rd Party Mac Developer Application: Jason Champion (G6PFF85X2R)" Sigmatizm.app --entitlements Sigmatizm.app/Contents/Resources/Entitlements.plist
+productbuild --sign "3rd Party Mac Developer Installer: Jason Champion (G6PFF85X2R)" --component Sigmatizm.app /Applications Sigmatizm.pkg
